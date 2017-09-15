e0c633b0d1c74f86a4bd8974ea295ee44fefffd9
z-gora <domdlapsa@gmail.com>
2017-09-15 13:55:19 +0200

    Show pages that are disambiguations.
    Links will have event handlers that load the proper meaning.

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
         // Send a request for an original extract and check language availability
         $.ajax( {
             url: 'https://en.wikipedia.org/w/api.php?callback=?',
-            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'pageids' : sourceID},
+            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks|pageprops', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'ppprop' : 'disambiguation', 'pageids' : sourceID},
             dataType: 'json',
             type: 'POST',
             headers: { 'Api-User-Agent': 'Example/1.0' },
@@ -82,35 +82,58 @@ $(document).ready(function() {
                 $.each(data['query']['pages'], function(index, value) {
                     sourceID = value['pageid'];
                 });
-                // Update all the info fields and the link
+                // Update the title
                 $("#search_box").val(data['query']['pages'][sourceID]['title']);
-                $("#from_extract").text(data['query']['pages'][sourceID]['extract']);
-                $("#from_link").attr("href", "https://en.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
-                $("#from_link").removeClass("disabled");
-                // Show the redirectio info dialog if needed
-                $("#redirect").hide();
-                if (data['query']['redirects'] !== undefined) {
-                    $("#redirect span").text(data['query']['redirects'][0]['from']);
-                    $("#redirect").show();
-                }
-                if (data['query']['pages'][sourceID]['langlinks'] !== undefined) {
-                    // Send request for translation if available
+                //Check whether page is disambiguation
+                if (data['query']['pages'][sourceID]['pageprops'] === undefined) {
+                    // If not, proceed normally
+                    $("#from_extract").text(data['query']['pages'][sourceID]['extract']);
+                    $("#from_link").attr("href", "https://en.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
+                    $("#from_link").removeClass("disabled");
+                    // Show the redirectio info dialog if needed
+                    $("#redirect").hide();
+                    if (data['query']['redirects'] !== undefined) {
+                        $("#redirect span").text(data['query']['redirects'][0]['from']);
+                        $("#redirect").show();
+                    }
+                    if (data['query']['pages'][sourceID]['langlinks'] !== undefined) {
+                        // Send request for translation if available
+                        $.ajax( {
+                            url: 'https://' + data['query']['pages'][sourceID]['langlinks'][0]['lang'] + '.wikipedia.org/w/api.php?callback=?',
+                            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'titles' : data['query']['pages'][sourceID]['langlinks'][0]['*']},
+                            dataType: 'json',
+                            type: 'POST',
+                            headers: { 'Api-User-Agent': 'Example/1.0' },
+                            success: function(transdata) {
+                                // Find ID of the translated article
+                                $.each(transdata['query']['pages'], function(index, value) {
+                                    transID = value['pageid'];
+                                });
+                                // Update all the info fields and the link
+                                $("#to_title").text(transdata['query']['pages'][transID]['title']);
+                                $("#to_extract").text(transdata['query']['pages'][transID]['extract']);
+                                $("#to_link").attr("href", "https://pl.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
+                                $("#to_link").removeClass("disabled");
+                            }
+                        });
+                    } else {
+                        $("#to_title").text("---");
+                        $("#to_extract").text("No translation found");
+                    }
+                } else {
+                    // If the page is a disambiguation, display the available meanings
                     $.ajax( {
-                        url: 'https://' + data['query']['pages'][sourceID]['langlinks'][0]['lang'] + '.wikipedia.org/w/api.php?callback=?',
-                        data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'titles' : data['query']['pages'][sourceID]['langlinks'][0]['*']},
+                        url: 'https://en.wikipedia.org/w/api.php?callback=?',
+                        data: {'action' : 'parse', 'format' : 'json', 'disableeditsection' : '1', 'disabletoc' : '1', 'pageid' : sourceID},
                         dataType: 'json',
                         type: 'POST',
                         headers: { 'Api-User-Agent': 'Example/1.0' },
-                        success: function(transdata) {
-                            // Find ID of the translated article
-                            $.each(transdata['query']['pages'], function(index, value) {
-                                transID = value['pageid'];
+                        success: function(disdata) {
+                            $("#from_extract").html(disdata['parse']['text']['*']);
+                            $("#disambigbox").remove();
+                            $("#from_extract .mw-parser-output a").click(function(e) {
+                                e.preventDefault();
                             });
-                            // Update all the info fields and the link
-                            $("#to_title").text(transdata['query']['pages'][transID]['title']);
-                            $("#to_extract").text(transdata['query']['pages'][transID]['extract']);
-                            $("#to_link").attr("href", "https://pl.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
-                            $("#to_link").removeClass("disabled");
                         }
                     });
                 }
