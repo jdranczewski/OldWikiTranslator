252a30426a0624785e2123ad07d984c2c71325f4
z-gora <domdlapsa@gmail.com>
2017-09-15 13:14:42 +0200

    Added basic translating ability
    From English to Polish for the time being

--- js/main.js
+++ js/main.js
@@ -53,11 +53,13 @@ $(document).ready(function() {
     });
     
     function loadTranslation(){
+        $("#from_link").addClass("disabled");
+        $("#to_link").addClass("disabled");
         sourceID = $("#autocomplete .selected").data("id");
         $("#autocomplete").empty().hide();
         $.ajax( {
             url: 'https://en.wikipedia.org/w/api.php?callback=?',
-            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts', 'exintro' : '', 'explaintext' : '', 'exsentences' : '3', 'redirects' : '', 'pageids' : sourceID},
+            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'pageids' : sourceID},
             dataType: 'json',
             type: 'POST',
             headers: { 'Api-User-Agent': 'Example/1.0' },
@@ -70,10 +72,30 @@ $(document).ready(function() {
                 });
                 $("#search_box").val(data['query']['pages'][sourceID]['title']);
                 $("#from_extract").text(data['query']['pages'][sourceID]['extract']);
+                $("#from_link").attr("href", "https://en.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
+                $("#from_link").removeClass("disabled");
                 $("#redirect").hide();
                 if (data['query']['redirects'] !== undefined) {
-                    $("#redirect span").text(data['query']['redirects'][0]['to'])
+                    $("#redirect span").text(data['query']['redirects'][0]['from']);
                     $("#redirect").show();
+                }
+                if (data['query']['pages'][sourceID]['langlinks'] !== undefined) {
+                    $.ajax( {
+                        url: 'https://' + data['query']['pages'][sourceID]['langlinks'][0]['lang'] + '.wikipedia.org/w/api.php?callback=?',
+                        data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'titles' : data['query']['pages'][sourceID]['langlinks'][0]['*']},
+                        dataType: 'json',
+                        type: 'POST',
+                        headers: { 'Api-User-Agent': 'Example/1.0' },
+                        success: function(transdata) {
+                            $.each(transdata['query']['pages'], function(index, value) {
+                                transID = value['pageid'];
+                            });
+                            $("#to_title").text(transdata['query']['pages'][transID]['title']);
+                            $("#to_extract").text(transdata['query']['pages'][transID]['extract']);
+                            $("#to_link").attr("href", "https://pl.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
+                            $("#to_link").removeClass("disabled");
+                        }
+                    });
                 }
             }
         });
