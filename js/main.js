26aacdc96b52986fa43d319f9c2379fda61c835e
z-gora <domdlapsa@gmail.com>
2017-10-25 23:08:07 +0100

    Languages in hash handling
    Also, loading messages

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             if ($("#search_box").val() !== "") {
                 // Send a request for search
                 $.ajax( {
-                    url: 'https://en.wikipedia.org/w/api.php?callback=?',
+                    url: 'https://' + hash_data[1] + '.wikipedia.org/w/api.php?callback=?',
                     data: {'action' : 'query', 'format' : 'json', 'list' : 'prefixsearch', 'pslimit' : 5, 'pssearch' : $("#search_box").val()},
                     dataType: 'json',
                     type: 'POST',
@@ -28,7 +28,8 @@ $(document).ready(function() {
                                 $(".selected").removeClass("selected");
                                 $(this).addClass("selected");
                                 // Load the translation when a suggestion clicked
-                                location.hash = $("#autocomplete .selected").text()
+                                hash_data[0] = $("#autocomplete .selected").text()
+                                location.hash = hash_data.join("//wt-")
                             });
                             $("#autocomplete").show();
                         } else {
@@ -69,12 +70,17 @@ $(document).ready(function() {
         $("#disambiguation").hide();
         $("#redirect").hide();
         $("#section").hide();
+        $("#from_extract").text("Loading...")
+        $("#to_extract").text("Loading...")
+        $("#to_title").text("Loading...")
         sourceID = $("#autocomplete .selected").data("id");
         $("#autocomplete").empty().hide();
+        // Parse the hash string
+        hash_data = location.hash.substr(1).split("//wt-")
         // Send a request for an original extract and check language availability
         $.ajax( {
-            url: 'https://en.wikipedia.org/w/api.php?callback=?',
-            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks|pageprops', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'ppprop' : 'disambiguation', 'titles' : location.hash.substr(1)},
+            url: 'https://' + hash_data[1] + '.wikipedia.org/w/api.php?callback=?',
+            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks|pageprops', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : hash_data[2], 'ppprop' : 'disambiguation', 'titles' : hash_data[0]},
             dataType: 'json',
             type: 'POST',
             headers: { 'Api-User-Agent': 'Example/1.0' },
@@ -91,7 +97,7 @@ $(document).ready(function() {
                 if (data['query']['pages'][sourceID]['pageprops'] === undefined) {
                     // If not, proceed normally
                     $("#from_extract").text(data['query']['pages'][sourceID]['extract']);
-                    $("#from_link").attr("href", "https://en.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
+                    $("#from_link").attr("href", "https://" + hash_data[1] + ".wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
                     $("#from_link").removeClass("disabled");
                     // Show the redirection info dialog if needed
                     if (data['query']['redirects'] !== undefined) {
@@ -100,8 +106,8 @@ $(document).ready(function() {
                     }
                     // Chceck whether disambiguation available
                     $.ajax( {
-                        url: 'https://en.wikipedia.org/w/api.php?callback=?',
-                        data: {'action' : 'query', 'format' : 'json', 'redirects' : '', 'titles' : location.hash.substr(1) + " (disambiguation)"},
+                        url: 'https://' + hash_data[1] + '.wikipedia.org/w/api.php?callback=?',
+                        data: {'action' : 'query', 'format' : 'json', 'redirects' : '', 'titles' : hash_data[0] + " (disambiguation)"},
                         dataType: 'json',
                         type: 'POST',
                         headers: { 'Api-User-Agent': 'Example/1.0' },
@@ -115,7 +121,7 @@ $(document).ready(function() {
                         // Send request for translation if available
                         $.ajax( {
                             url: 'https://' + data['query']['pages'][sourceID]['langlinks'][0]['lang'] + '.wikipedia.org/w/api.php?callback=?',
-                            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : 'pl', 'titles' : data['query']['pages'][sourceID]['langlinks'][0]['*']},
+                            data: {'action' : 'query', 'format' : 'json', 'prop' : 'extracts|langlinks', 'exintro' : '', 'explaintext' : '', 'exchars' : '300', 'redirects' : '', 'lllang' : hash_data[2], 'titles' : data['query']['pages'][sourceID]['langlinks'][0]['*']},
                             dataType: 'json',
                             type: 'POST',
                             headers: { 'Api-User-Agent': 'Example/1.0' },
@@ -131,7 +137,7 @@ $(document).ready(function() {
                                     $("#section").show();
                                 }
                                 $("#to_extract").text(transdata['query']['pages'][transID]['extract']);
-                                $("#to_link").attr("href", "https://pl.wikipedia.org/wiki/" + transdata['query']['pages'][transID]['title']);
+                                $("#to_link").attr("href", "https://" + hash_data[2] + ".wikipedia.org/wiki/" + transdata['query']['pages'][transID]['title']);
                                 $("#to_link").removeClass("disabled");
                             }
                         });
@@ -142,7 +148,7 @@ $(document).ready(function() {
                 } else {
                     // If the page is a disambiguation, display the available meanings
                     $.ajax( {
-                        url: 'https://en.wikipedia.org/w/api.php?callback=?',
+                        url: 'https://' + hash_data[1] + '.wikipedia.org/w/api.php?callback=?',
                         data: {'action' : 'parse', 'format' : 'json', 'disableeditsection' : '1', 'disabletoc' : '1', 'pageid' : sourceID},
                         dataType: 'json',
                         type: 'POST',
@@ -153,10 +159,12 @@ $(document).ready(function() {
                             $("#from_extract table").remove();
                             $("#from_extract a").click(function(e) {
                                 e.preventDefault();
-                                location.hash = $(this).attr("href").substr(6);
+                                hash_data[0] = $(this).attr("href").substr(6)
+                                location.hash = hash_data.join("//wt-");
                             });
-                            $("#from_link").attr("href", "https://en.wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
+                            $("#from_link").attr("href", "https://" + hash_data[1] + ".wikipedia.org/wiki/" + data['query']['pages'][sourceID]['title']);
                             $("#from_link").removeClass("disabled");
+                            $("#to_title").text("---");
                             $("#to_extract").text("Choose one of the meanings on the left to see the translation.");
                         }
                     });
@@ -165,16 +173,23 @@ $(document).ready(function() {
         });
     }
     
-    // Load the language preference from a cookie
-    cookie_lang = monster.get($("#short_page_name").val())
-    if (cookie_lang !== null) {
-        cookie_lang == cookie_lang.split(";")
-    }
-    
+    // Hash change handling
     $(window).on('hashchange', loadTranslation);
     
     if (location.hash.length) {
+        // Parse the hash string
+        hash_data = location.hash.substr(1).split("//wt-")
         loadTranslation();
+    } else {
+        // If no data in hash onload, try cookies for language preference
+        cookie_lang = monster.get("language_cookie")
+        if (cookie_lang !== null) {
+            cookie_lang == cookie_lang.split(";")
+            hash_data = ["", cookie_lang[0], cookie_lang[1]]
+        } else {
+            // Fall back to Polish and English
+            hash_data = ["", "pl", "en"]
+        }
     }
     
     // Load up the disambiguation
